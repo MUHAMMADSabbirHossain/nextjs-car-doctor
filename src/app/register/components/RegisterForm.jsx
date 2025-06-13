eb4da1340fb3ef2e9c4311627aab6ca61aaf192e
Muhammad Sabbir Hossain Bin Ahmad <your_email@example.com>
2025-06-13 11:06:45 +0600

    feat: update RegisterForm with improved styling and add navigation to login page. ✅👍

--- src/app/register/components/RegisterForm.jsx
+++ src/app/register/components/RegisterForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { registerUser } from '@/app/action/auth/registerUser';
+import Link from 'next/link';
 import React from 'react'
 
 export default function RegisterForm() {
@@ -10,27 +11,55 @@ export default function RegisterForm() {
         const data = Object.fromEntries(formData);
         console.log(data);
 
-        await registerUser(data);
+        await registerUser(...data);
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" placeholder='Name' />
-            </div>
-
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder='Email' />
-            </div>
-
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" placeholder='Password' />
-            </div>
-
-            <button type="submit">Register</button>
+        <form onSubmit={handleSubmit} className="w-full max-w-lg space-y-8">
+            <label className="form-control w-full">
+                <div className="label w-full">
+                    <span className="label-text  font-bold">Name</span>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    name="name"
+                />
+            </label>
+            <label className="form-control w-full">
+                <div className="label w-full">
+                    <span className="label-text  font-bold">Email</span>
+                </div>
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                />
+            </label>
+            <label className="form-control w-full">
+                <div className="label w-full">
+                    <span className="label-text font-bold">Password</span>
+                </div>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                />
+            </label>
+            <button className="w-full h-12 bg-orange-500 text-white font-bold">
+                Sign Up
+            </button>
+            <p className="text-center">Or Sign In with</p>
+            {/* <SocialLogin /> */}
+            <p className="text-center">
+                Don't Have an account?{" "}
+                <Link href="/login" className="text-orange-500 font-bold">
+                    Login
+                </Link>
+            </p>
         </form>
-    )
+    );
 }
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import RegisterForm from './components/RegisterForm'
+import Image from "next/image";
+
+import React from "react";
+
+import RegisterForm from "./components/RegisterForm";
 
 export default function RegisterPage() {
     return (
-        <section>
-            <h1>Register</h1>
+        <>
+            <h1 className="text-3xl font-bold text-center my-8">Register</h1>
+            <section className="container mx-auto grid grid-cols-12">
+                {/* Left Section */}
+                <div className="col-span-12 md:col-span-6 flex justify-center items-center">
+                    <Image
+                        className="hidden md:block"
+                        src={"/assets/images/login/login.svg"}
+                        width={460}
+                        height={500}
+                        alt={"Authentication Image"}
+                    />
+                </div>
 
-            <RegisterForm />
-        </section>
-    )
+                {/* Right Section */}
+                <div className="col-span-12 md:col-span-6 flex justify-center items-center">
+                    <RegisterForm />
+                </div>
+            </section>
+        </>
+    );
 }
